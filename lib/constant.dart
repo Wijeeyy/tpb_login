2b42fdf43f7a62775a45d90fce249d0d77fde41b
LEGION7\wijee <wizmaurer17@gmail.com>
2023-07-27 00:37:14 +0700

    first commit

---
+++ lib/constant.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color whiteColor = Color(0xfffffffff);
+Color textColor = Color.fromARGB(255, 49, 92, 135);
+Color secondaryTextColor = Color.fromARGB(255, 113, 133, 119);
+Color buttonColor = Color.fromARGB(255, 15, 7, 132);
+Color primaryButtonColor = Color.fromARGB(255, 225, 211, 211);
+Color tncButtonColor = Color.fromARGB(255, 42, 4, 165);
+Color greyColor = Color.fromARGB(255, 182, 193, 195);
+
+TextStyle whiteTextStyle = GoogleFonts.dmSans(
+  color: whiteColor,
+);
+TextStyle textTextStyle = GoogleFonts.dmSans(
+  color: textColor,
+);
+TextStyle secondaryTextStyle = GoogleFonts.dmSans(
+  color: secondaryTextColor,
+);
+TextStyle tncTextStyle = GoogleFonts.dmSans(
+  color: tncButtonColor,
+);
+TextStyle greyTextStyle = GoogleFonts.dmSans(
+  color: greyColor,
+);
+
+FontWeight bold = FontWeight.bold;
